a687115b60a9e8d6567eaff2e816ef1d9e9fccfd
lexucuflexu <alex.tenche@gmail.com>
2014-09-05 21:40:29 +0300

    change.php added
    where is candge.php

--- change.php
+++ change.php
@@ -3,4 +3,6 @@ $id = $_REQUEST['id'];
 $newname = $_REQUEST['newname'];
 $newemail = $_REQUEST['newemail'];
 $newpassword = $_REQUEST['newpassword'];
+
+mysqli_connect("localhost","root","termopane","testsite")
 ?>
