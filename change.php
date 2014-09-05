0eec587bfa6ead9767cde7d22307fe420c26f315
lexucuflexu <alex.tenche@gmail.com>
2014-09-05 21:47:19 +0300

    gata updateu

--- change.php
+++ change.php
@@ -4,5 +4,13 @@ $newname = $_REQUEST['newname'];
 $newemail = $_REQUEST['newemail'];
 $newpassword = $_REQUEST['newpassword'];
 
-mysqli_connect("localhost","root","termopane","testsite")
+$link = mysqli_connect("localhost","root","termopane","testsite");
+
+mysqli_query($link, "UPDATE users SET name='$newname', email='$newemail', password='$newpassword'
+			  WHERE id = '$id'");
+			  
+echo "Your values have been updated successfuly!";
+mysqli_close($link);
+
+include('links.php');
 ?>
