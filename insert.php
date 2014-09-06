548bba661e8f0a499877dff2cda91ba7f880f44a
lexucuflexu <alex.tenche@gmail.com>
2014-09-06 18:23:15 +0300

    check if name exists

--- insert.php
+++ insert.php
@@ -13,8 +13,17 @@
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
 	
+	$username = mysqli_query($link,"SELECT name FROM users WHERE name='$name'");
+	$count = mysqli_num_rows($username);
+	
+	if( $count != 0 ){
+		die("<b>ERROR: Name already exists! Please type another name</b>");
+		include('links.php');
+	}
+	
 	mysqli_query($link,"INSERT INTO users (name,email,password) VALUES ('$name','$email','$password')");
 	
+	echo "You have succesfully registred";
 	$registered = mysqli_affected_rows($link);
 	echo "$registered was inserted";
 	
