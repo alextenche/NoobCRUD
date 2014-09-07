5aed43223cafff25c00f42ea8f2895e69a7ecb38
lexucuflexu <alex.tenche@gmail.com>
2014-09-07 15:10:35 +0300

    added search

--- search.php
+++ search.php
@@ -13,18 +13,49 @@
     </form>
 	</center>	
 	<hr>
+	<u>Results:</u>&nbsp;
 	
 	<?php
 	
 	if(isset($_REQUEST['submit'])){
 		
 		$search = $_GET['search'];
-		$terms = explode(" ",$search);
+		$terms = explode(" ",$search); //returns an array
 		$query = "SELECT * FROM users WHERE ";
 		
 		$i = 0;
-		foreach(){}
-	
+		foreach($terms as $each){
+			$i++;
+			if($i==1){
+				$query .= "name LIKE '%$each%' ";
+			} else {
+				$query .= "OR name LIKE '%$each%' ";
+			}
+		}
+		
+		$link = mysqli_connect("localhost", "root", "termopane", "testsite");	
+		if (mysqli_connect_errno()){ echo "Failed to connect to MySQL: " . mysqli_connect_error();}
+		
+		$query = mysqli_query($link, $query);
+		$num = mysqli_num_rows($query);
+		
+		if($num > 0 && $search != ""){
+			
+			echo "$num result(s) found for <b>$search</b>";
+			
+			while($row = mysqli_fetch_assoc($query)){
+				$id = $row['id'];
+				$name =$row['name'];
+				$email =$row['email'];
+				$password =$row['password'];
+				
+				echo "<br><h3>name: $name(id:$id)</h3>email: $email<br>$email<br>password: $password<br>";
+			}
+		} else {
+			echo "No results found.";
+		}
+		
+		mysqli_close($link);
 	} else {
 		echo "Please type any word for serch.";
 	}
@@ -35,7 +66,7 @@
 	
 	
 	
-	
+	include('links.php');
 	?>
 	
 	
