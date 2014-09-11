860c44611c1c7982da8439bd2eae80de04ab49d4
lexucuflexu <lexucuflexu@gmail.com>
2014-09-12 00:40:54 +0300

    cookies and remember me

--- home.php
+++ home.php
@@ -1,30 +1,46 @@
-<!doctype html>
+<?php
+if(isset($_COOKIE['testsite'])){
+	
+	header('Location:enter.php');
+	
+} else {
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-</head>
-<body>
-	<h1><center>Welcome to CRUD control center</center></h1>
-    <center>Please login...
-	<form method="post" action="login.php">
-        <table border="0" width="10%">
-			<tr>
-				<td width="10%">Name:</td>
-				<td><input type="text" name="name" maxlength="15"/></td>
-            </tr>
-			<tr>
-				<td  width="10%">Password: </td>
-				<td><input type="password" name="password" maxlength="15"/><td>
-			</tr>
-		</table>
-        <p>
-			<input type="submit" value="login"/>
-		</p>
-    </form>
-	<a href="form.php">Register?</a>
-	</center>
+	echo "
+	<!doctype html>
+	<html lang='en'>
+	<head>
+		<meta charset='utf-8'>
+		<title>Home</title>
+	</head>
+	<body>
+		<h1><center>Welcome to CRUD control center</center></h1>
+		<center>Please login...</center>
+		<div align='center'>
+			<form method='post' action='login.php'>
+			<table border='0' width='10%'>
+				<tr>
+					<td>Name:</td>
+					<td><input type='text' name='name' maxlength='15'/></td>
+				</tr>
+				<tr>
+					<td>Password: </td>
+					<td><input type='password' name='password' maxlength='15'/><td>
+				</tr>
+				<tr>
+					<td>Password: </td>
+					<td><input type='password' name='password' maxlength='15'/><td>
+				</tr>
+			</table>
+			<p>
+				<input type='submit' value='login'/>
+			</p>
+			</form>
+		<a href="form.php">Register?</a>
+	</div>
+
 
 </body>
 </html>
+
+}
+?>
