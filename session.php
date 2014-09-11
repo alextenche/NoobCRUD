e5c7e4a92207bb151d6d52600be55e79ec10a870
lexucuflexu <alex.tenche@gmail.com>
2014-09-11 21:50:48 +0300

    nu mai stiu

--- session.php
+++ session.php
@@ -1,16 +1,18 @@
 <?php
-	if(!isset($_SESSION['name'])){
-		echo "not a session";
-	} else {
-		$dir = "profiles/" . $_SESSION['name'] . "/images/";
-		$open = opendir($dir);
+$time = time();
+$date = date('d F, Y, g: i: s a');
+echo "Today is ".$date;
+
+if(!isset($_SESSION['name'])){
+	echo "not a session";
+} else {
+	$dir = "profiles/" . $_SESSION['name'] . "/images/";
+	$open = opendir($dir);
 		
-		while(($file = readdir($open)) != false){
-			if(($file != ".") && ($file != "..") && ($file != "Thumbs.db")){
-				echo "<img border='1' width='70' height='70' src='$dir/$file'/>";
-			}
+	while(($file = readdir($open)) != false){
+		if(($file != ".") && ($file != "..") && ($file != "Thumbs.db")){
+			echo "<img border='1' width='70' height='70' src='$dir/$file'/>";
 		}
-		
-		echo "&nbsp;<b>" . $_SESSION['name'] . "</b>" . "'s session<br><a href='logout.php'>Logout</a><hr>";
 	}
-?>
+	echo "&nbsp;<b>" . $_SESSION['name'] . "</b>" . "'s session<br><a href='logout.php'>Logout</a><hr>";
+}?>
